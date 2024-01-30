fdbd6efce93436c758cada4897830ad4ea786a42
jamiegibney <jamiegibneyuk@gmail.com>
2024-01-30 13:09:55 +0000

    changed autosave message

--- after/plugin/autosave.lua
+++ after/plugin/autosave.lua
@@ -1,19 +1,20 @@
 require("auto-save").setup {
     execution_message = {
         message = function()
-            return "File saved"
+            return "File autosaved"
         end,
         dim = 0.50,
     },
-    -- prevent autosaving in non-modifiable and oil buffers
+    -- prevent autosaving in non-modifiable and oil/harpoon buffers
     condition = function(buf)
         local fn = vim.fn
-        local utils = require "auto-save.utils.data"
+        local utils = require("auto-save.utils.data")
 
         if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), { "oil", "harpoon", }) then
             return true
         end
         return false
     end,
+    -- delay before autosave is triggered
     debounce_delay = 130,
 }
