3130ca2f48215dfa772353348e414145ed0ae004
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-03-20 23:28:35 +0000

    renamed autopairs and trouble config files

--- after/plugin/_trouble.lua
+++ after/plugin/_trouble.lua
@@ -1,14 +1,14 @@
 -- goto previous workspace diagnostic
 vim.keymap.set("n", "d[", function()
-    local tr = require("trouble")
+    local tr = require("after.plugin._trouble")
     tr.open()
-    require("trouble").previous({ jump = true, skip_groups = true })
+    require("after.plugin._trouble").previous({ jump = true, skip_groups = true })
     tr.close()
 end)
 
 -- goto next workspace diagnostic
 vim.keymap.set("n", "d]", function()
-    local tr = require("trouble")
+    local tr = require("after.plugin._trouble")
     tr.open()
     tr.next({ jump = true, skip_groups = true })
     tr.close()
