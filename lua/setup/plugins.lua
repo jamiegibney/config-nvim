a34942d649a4d3b2508fc22e27c25a9b0b0c1827
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-05 02:30:41 +0100

    readded melange colorscheme (fallback)

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     },
 
     -- themes
-    -- { "savq/melange-nvim", event = "VeryLazy", },
+    { "savq/melange-nvim", event = "VeryLazy", },
     -- { "rose-pine/neovim", name = "rose-pine", event = "VeryLazy", },
     -- { "ellisonleao/gruvbox.nvim", event = "VeryLazy", },
 
