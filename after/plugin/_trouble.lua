d2dd0297d350e96976530cabf635e43412196836
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-20 00:14:05 +0100

    fix: removed trouble mappings

--- after/plugin/_trouble.lua
+++ after/plugin/_trouble.lua
@@ -1,15 +1,15 @@
--- goto previous workspace diagnostic
-vim.keymap.set("n", "d[", function()
-    local tr = require("after.plugin._trouble")
-    tr.open()
-    require("after.plugin._trouble").previous({ jump = true, skip_groups = true })
-    tr.close()
-end)
-
--- goto next workspace diagnostic
-vim.keymap.set("n", "d]", function()
-    local tr = require("after.plugin._trouble")
-    tr.open()
-    tr.next({ jump = true, skip_groups = true })
-    tr.close()
-end)
+-- -- goto previous workspace diagnostic
+-- vim.keymap.set("n", "d[", function()
+--     local tr = require("after.plugin._trouble")
+--     tr.open()
+--     require("after.plugin._trouble").previous({ jump = true, skip_groups = true })
+--     tr.close()
+-- end)
+--
+-- -- goto next workspace diagnostic
+-- vim.keymap.set("n", "d]", function()
+--     local tr = require("after.plugin._trouble")
+--     tr.open()
+--     tr.next({ jump = true, skip_groups = true })
+--     tr.close()
+-- end)
