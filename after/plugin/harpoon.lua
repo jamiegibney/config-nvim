2ca42a1025e92a08aa0c6f9865d04bb499f2f7d3
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-07-27 15:53:52 +0100

    refactor: cleaned up harpoon file

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -31,106 +31,23 @@ map("<leader>af", function()
     print("File added to Harpoon")
 end)
 
---[[ -- "goto 1"
-map("<A-1>", function()
-    harpoon:list():select(1)
-end)
--- "goto 2"
-map("<A-2>", function()
-    harpoon:list():select(2)
-end)
--- "goto 3"
-map("<A-3>", function()
-    harpoon:list():select(3)
-end)
--- "goto 4"
-map("<A-4>", function()
-    harpoon:list():select(4)
-end)
--- "goto 5"
-map("<A-5>", function()
-    harpoon:list():select(5)
-end)
--- "goto 6"
-map("<A-6>", function()
-    harpoon:list():select(6)
-end)
--- "goto 7"
-map("<A-7>", function()
-    harpoon:list():select(7)
-end)
--- "goto 8"
-map("<A-8>", function()
-    harpoon:list():select(8)
-end)
--- "goto 9"
-map("<A-9>", function()
-    harpoon:list():select(9)
-end)
-
--- alternative command
 -- "goto 1"
-map("<C-6>", function()
+map("<C-j>", function()
     harpoon:list():select(1)
 end)
 -- "goto 2"
-map("<C-7>", function()
+map("<C-k>", function()
     harpoon:list():select(2)
 end)
 -- "goto 3"
-map("<C-8>", function()
+map("<C-l>", function()
     harpoon:list():select(3)
 end)
 -- "goto 4"
-map("<C-9>", function()
+map("<C-;>", function()
     harpoon:list():select(4)
 end)
 -- "goto 5"
-map("<C-0>", function()
+map("<C-'>", function()
     harpoon:list():select(5)
-end) ]]
-
--- alternative homerow
-if vim.g.use_dvorak_bindings then
-    -- "goto 1"
-    map("<C-h>", function()
-        harpoon:list():select(1)
-    end)
-    -- "goto 2"
-    map("<C-t>", function()
-        harpoon:list():select(2)
-    end)
-    -- "goto 3"
-    map("<C-n>", function()
-        harpoon:list():select(3)
-    end)
-    -- "goto 4"
-    map("<C-s>", function()
-        harpoon:list():select(4)
-    end)
-    -- "goto 5"
-    map("<C-/>", function()
-        harpoon:list():select(5)
-    end)
-else
-    -- "goto 1"
-    map("<C-j>", function()
-        harpoon:list():select(1)
-    end)
-    -- "goto 2"
-    map("<C-k>", function()
-        harpoon:list():select(2)
-    end)
-    -- "goto 3"
-    map("<C-l>", function()
-        harpoon:list():select(3)
-    end)
-    -- "goto 4"
-    map("<C-;>", function()
-        harpoon:list():select(4)
-    end)
-    -- "goto 5"
-    map("<C-'>", function()
-        harpoon:list():select(5)
-    end)
-end
+end)
