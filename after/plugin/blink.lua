c33ddc1a0035ee793cae4c5444a5bcdfbb45bbec
Jamie Gibney <jamiegibneyuk@gmail.com>
2025-04-02 00:34:38 +0100

    chore: disabled blink config

--- after/plugin/blink.lua
+++ after/plugin/blink.lua
@@ -1,84 +1,83 @@
--- local blink = require("blink-cmp")
---
--- require("blink-cmp").setup({
---     keymap = {
---         -- show = { "<C-n>", "<C-p>" },
---         hide = "<C-e>",
---         accept = "<C-y>",
---         select_prev = "<C-p>",
---         select_next = "<C-n>",
---
---         -- show_documentation = { "<C-n>", "<C-p>" },
---         hide_documentation = "<C-\\>",
---         scroll_documentation_up = "<C-b>",
---         scroll_documentation_down = "<C-f>",
---
---         snippet_forward = "<C-f>",
---         snippet_backward = "<S-C-f>",
---     },
---
---     sources = {
---         providers = {
---             {
---                 {
---                     "blink.cmp.sources.lsp",
---                     name = "LSP",
---                 },
---                 {
---                     "blink.cmp.sources.path",
---                     name = "Path",
---                 },
---                 {
---                     "blink.cmp.sources.snippets",
---                     name = "Snip",
---                     score_offset = -2
---                 },
---             },
---             -- { { "blink.cmp.sources.buffer" } },
---         },
---     },
---
---     kind_icons = {
---         Text = "  ",
---         Method = "󰊕 ",
---         Function = "󰊕 ",
---         Constructor = "󰊕 ",
---
---         Field = "  ",
---         Variable = "  ",
---         Property = "  ",
---
---         Class = "  ",
---         Interface = "  ",
---         Struct = "  ",
---         Module = "  ",
---
---         Unit = "  ",
---         Value = "󰎠  ",
---         Enum = "   ",
---         EnumMember = "  ",
---
---         -- Keyword = "󰻾 ",
---         Keyword = "󰌋  ",
---         Constant = "󰏿  ",
---
---         Snippet = "  ",
---         Color = "  ",
---         File = "  ",
---         Reference = "  ",
---         Folder = "  ",
---         Event = "  ",
---         Operator = "  ",
---         TypeParameter = "  ",
---     },
---
---     highlight = {
---         use_nvim_cmp_as_default = true,
---     },
---
---     trigger = {
---         signature_help = {
---             enabled = false
---         }
---     },
--- })
+--[[ require("blink-cmp").setup({
+    keymap = {
+        preset = "default",
+        ["<C-e>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<C-f>"] = { "snippet_forward" },
+        ["<S-C-f>"] = { "snippet_backward" },
+    },
+
+    completion = {
+        accept = {
+            auto_brackets = {
+                enabled = false,
+            }
+        },
+
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 10,
+        },
+
+        trigger = {
+            signature_help = {
+                enabled = false
+            }
+        },
+
+        ghost_text = {
+            enabled = true,
+        },
+    },
+
+    sources = {
+        default = { "lsp", "path", "snippets", },
+        per_filetype = {
+            oil = {},
+            txt = { "buffer", "path", },
+        },
+    },
+
+    appearance = {
+        nerd_font_variant = "mono",
+        kind_icons = {
+            Text = "  ",
+            Method = "󰊕 ",
+            Function = "󰊕 ",
+            Constructor = "󰊕 ",
+
+            Field = "  ",
+            Variable = "  ",
+            Property = "  ",
+
+            Class = "  ",
+            Interface = "  ",
+            Struct = "  ",
+            Module = "  ",
+
+            Unit = "  ",
+            Value = "󰎠  ",
+            Enum = "   ",
+            EnumMember = "  ",
+
+            -- Keyword = "󰻾 ",
+            Keyword = "󰌋  ",
+            Constant = "󰏿  ",
+
+            Snippet = "  ",
+            Color = "  ",
+            File = "  ",
+            Reference = "  ",
+            Folder = "  ",
+            Event = "  ",
+            Operator = "  ",
+            TypeParameter = "  ",
+        },
+    },
+})
+
+local function hl(group, opts)
+    vim.api.nvim_set_hl(0, group, opts)
+end
+
+hl("BlinkCmpGhostText", { fg = "#bbbbbb" }) ]]
